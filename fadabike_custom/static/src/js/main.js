7f0b14e1f2aeb70b0080211347f73c43575cd980
Stéphan Sainléger <contact@stephansainleger.fr>
2023-11-06 17:34:28 +0100

    [NEW] fadabike_custom: create add-on

---
+++ fadabike_custom/static/src/js/main.js
@@ -0,0 +1,22 @@
+openerp.fadabike_custom = function (instance) {
+
+    var _t = instance.web._t,
+    _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+
+    // instance.web.views.add('kanban', 'instance.fadabike_custom.MyView');
+
+    // instance.fadabike_custom.MyView = instance.web.View.extend({
+    //     template: "MyView",
+    //     display_name: _lt('MyView'),
+    //     default_nr_columns: 1,
+    //     view_type: "myview",
+    //     quick_create_class: "instance.fadabike_custom.QuickCreate",
+    //     number_of_color_schemes: 10,
+    //     init: function (parent, dataset, view_id, options) {
+    //         this._super(parent, dataset, view_id, options);
+    //     }
+    // });
+
+};
